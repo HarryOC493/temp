b62edba48991e3b45cb2b3da478b5ccf8e1cd561
Harry O'Connor <harryoconnor007@gmail.com>
2023-09-24 17:33:11 +0100

    Update test.py

--- test.py
+++ test.py
@@ -10,27 +10,25 @@ def request_sensor_data():
 
 # Function to read and parse sensor data from the Arduino
 def read_sensor_data():
-    data = ser.readline().decode().strip()
-    sensor_values = data.split('\t')
-    try:
-        sensor_values = [float(val) for val in sensor_values]
+    line = ser.readline().decode().strip()
+    if line.startswith("SENSOR_DATA:"):
+        data = line[len("SENSOR_DATA:"):]
+        sensor_values = [float(val) for val in data.split('\t')]
         return sensor_values
-    except ValueError as e:
-        print("Error converting sensor data:", e)
-        return None
-
+    return None  # Return None if the line doesn't contain sensor data
 
 # Main loop to continuously request and read sensor data
 try:
     while True:
         request_sensor_data()
         sensor_values = read_sensor_data()
-        
-        # Store sensor values in a list (sensor_values)
-        # You can access individual sensor values like sensor_values[0], sensor_values[1], etc.
-        
-        # Print the sensor values for demonstration
-        print("Sensor Values:", sensor_values)
+
+        if sensor_values is not None:
+            # Store sensor values in a list (sensor_values)
+            # You can access individual sensor values like sensor_values[0], sensor_values[1], etc.
+
+            # Print the sensor values for demonstration
+            print("Sensor Values:", sensor_values)
 
         # Add a delay to control the data update rate (in seconds)
         time.sleep(1)  # Adjust the sleep duration as needed
