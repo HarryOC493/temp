8110756e32437f1e54f52355047d9f4718f4cfe3
Harry O'Connor <harryoconnor007@gmail.com>
2023-09-24 19:08:22 +0100

    Update read.py

--- read.py
+++ read.py
@@ -2,27 +2,41 @@ import serial
 import time
 
 # Replace 'YOUR_SERIAL_PORT' with the actual serial port of your Arduino (e.g., '/dev/ttyUSB0' or 'COM3')
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
 # Function to send a request for sensor data to the Arduino
 def request_sensor_data():
     ser.write(b'R')
-    time.sleep(20)
-    print("Should have initalised by now")
 
-# Main loop to continuously request and read data from the Arduino
+# Function to read and parse sensor data from the Arduino
+def read_sensor_data():
+    line = ser.readline().decode().strip()
+    if line.startswith("SENSOR_DATA:"):
+        data = line[len("SENSOR_DATA:"):]
+        sensor_values = [float(val) for val in data.split('\t')]
+        return sensor_values
+    return None  # Return None if the line doesn't contain sensor data
+
+# Main loop to continuously request and read sensor data
 try:
+    # Open the serial connection only once
+    if not ser.is_open:
+        ser.open()
+
     while True:
         request_sensor_data()
+        sensor_values = read_sensor_data()
 
-        # Read and print all output from the Arduino
-        while ser.in_waiting:
-            line = ser.readline().decode().strip()
-            print(line)
+        if sensor_values is not None:
+            # Store sensor values in a list (sensor_values)
+            # You can access individual sensor values like sensor_values[0], sensor_values[1], etc.
+
+            # Print the sensor values for demonstration
+            print("Sensor Values:", sensor_values)
 
         # Add a delay to control the data update rate (in seconds)
         time.sleep(1)  # Adjust the sleep duration as needed
 
 except KeyboardInterrupt:
     print("Exiting...")
-    ser.close()
+    ser.close()  # Close the serial connection when exiting
