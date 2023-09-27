1c8b3044a599909b48e5975c328186e9e57ff35f
Harry O'Connor <harryoconnor007@gmail.com>
2023-09-27 21:09:07 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,20 +4,15 @@ import serial
 arduino_port = "/dev/ttyACM0"  # Linux example, may vary on Windows or macOS
 
 # Initialize serial communication with the Arduino
-ser = serial.Serial(arduino_port, baudrate=9600, timeout=1)
+ser = serial.Serial(arduino_port, baudrate=115200, timeout=1)
 
 try:
     while True:
         # Read the incoming data
         line = ser.readline().decode().strip()
 
-        # Check if the data starts with the specified header
-        if line.startswith("SENSOR_DATA:"):
-            # Process the sensor data (remove the header and convert to int)
-            sensor_data = int(line[len("SENSOR_DATA:"):])
-            print("Received Sensor Data:", sensor_data)
-
-        # Add other code here as needed...
+        # Print all serial data received from the Arduino
+        print("Arduino Data:", line)
 
 except KeyboardInterrupt:
     print("Keyboard interrupt detected. Exiting...")
