39c445b7558143144441111e3a50b0fa7556378d
Harry O'Connor <harryoconnor007@gmail.com>
2023-09-30 21:18:18 +0100

    Update read.py

--- read.py
+++ read.py
@@ -3,4 +3,4 @@ arduino = serial.Serial('/dev/ttyACM0', 115200, timeout=.1)
 while True:
 	data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
 	if data:
-		print data
+		print(data)
