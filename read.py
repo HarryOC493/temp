02c29dec44d331885a889565320bca7ea2d7be9a
Harry O'Connor <harryoconnor007@gmail.com>
2023-09-30 21:38:40 +0100

    Update read.py

--- read.py
+++ read.py
@@ -1,8 +1,10 @@
 import serial
+import time
 arduino = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 while True:
 	data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
 	if data:
 		print(data)
 		arduino.flushInput()  # Clear the input buffer
+		time.sleep(0.2)
 
