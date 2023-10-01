ccff733f7276c98c6081add7d3db952565532135
Harry O'Connor <harryoconnor007@gmail.com>
2023-10-01 16:31:03 +0100

    Update read.py

--- read.py
+++ read.py
@@ -1,82 +1,46 @@
-'''
-        Read Gyro and Accelerometer by Interfacing Raspberry Pi with MPU6050 using Python
-    http://www.electronicwings.com
-'''
-import smbus            #import SMBus module of I2C
-from time import sleep          #import
- 
-#some MPU6050 Registers and their Address
-PWR_MGMT_1   = 0x6B
-SMPLRT_DIV   = 0x19
-CONFIG       = 0x1A
-GYRO_CONFIG  = 0x1B
-INT_ENABLE   = 0x38
-ACCEL_XOUT_H = 0x3B
-ACCEL_YOUT_H = 0x3D
-ACCEL_ZOUT_H = 0x3F
-GYRO_XOUT_H  = 0x43
-GYRO_YOUT_H  = 0x45
-GYRO_ZOUT_H  = 0x47
- 
- 
-def MPU_Init():
-    #write to sample rate register
-    bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
-     
-    #Write to power management register
-    bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
-     
-    #Write to Configuration register
-    bus.write_byte_data(Device_Address, CONFIG, 0)
-     
-    #Write to Gyro configuration register
-    bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
-     
-    #Write to interrupt enable register
-    bus.write_byte_data(Device_Address, INT_ENABLE, 1)
- 
-def read_raw_data(addr):
-    #Accelero and Gyro value are 16-bit
-        high = bus.read_byte_data(Device_Address, addr)
-        low = bus.read_byte_data(Device_Address, addr+1)
-     
-        #concatenate higher and lower value
-        value = ((high << 8) | low)
-         
-        #to get signed value from mpu6050
-        if(value > 32768):
-                value = value - 65536
+import smbus
+import time
+
+# Define constants
+I2C_BUS = 1  # Use bus 1 for I2C communication on the Jetson Nano
+MPU6050_ADDRESS = 0x68  # MPU6050 I2C address
+
+# MPU6050 register addresses
+MPU6050_REGISTER_ACCEL_XOUT_H = 0x3B
+MPU6050_REGISTER_PWR_MGMT_1 = 0x6B
+
+# Initialize I2C bus
+i2c = smbus.SMBus(I2C_BUS)
+
+# Function to read 16-bit signed integer (two's complement) from MPU6050
+def read_i2c_word(register):
+    high_byte = i2c.read_byte_data(MPU6050_ADDRESS, register)
+    low_byte = i2c.read_byte_data(MPU6050_ADDRESS, register + 1)
+    value = (high_byte << 8) | low_byte
+    if value >= 0x8000:
+        return -((65535 - value) + 1)
+    else:
         return value
- 
- 
-bus = smbus.SMBus(1)    # or bus = smbus.SMBus(0) for older version boards
-Device_Address = 0x68   # MPU6050 device address
-sleep(1)
-MPU_Init()
- 
-print (" Reading Data of Gyroscope and Accelerometer")
- 
-while True:
-     
-    #Read Accelerometer raw value
-    acc_x = read_raw_data(ACCEL_XOUT_H)
-    acc_y = read_raw_data(ACCEL_YOUT_H)
-    acc_z = read_raw_data(ACCEL_ZOUT_H)
-     
-    #Read Gyroscope raw value
-    gyro_x = read_raw_data(GYRO_XOUT_H)
-    gyro_y = read_raw_data(GYRO_YOUT_H)
-    gyro_z = read_raw_data(GYRO_ZOUT_H)
-     
-    #Full scale range +/- 250 degree/C as per sensitivity scale factor
-    Ax = acc_x/16384.0
-    Ay = acc_y/16384.0
-    Az = acc_z/16384.0
-     
-    Gx = gyro_x/131.0
-    Gy = gyro_y/131.0
-    Gz = gyro_z/131.0
-     
- 
-    print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az)     
-    sleep(1)
+
+# Configure MPU6050
+i2c.write_byte_data(MPU6050_ADDRESS, MPU6050_REGISTER_PWR_MGMT_1, 0)
+
+try:
+    while True:
+        # Read accelerometer and gyroscope data
+        accel_x = read_i2c_word(MPU6050_REGISTER_ACCEL_XOUT_H)
+        accel_y = read_i2c_word(MPU6050_REGISTER_ACCEL_XOUT_H + 2)
+        accel_z = read_i2c_word(MPU6050_REGISTER_ACCEL_XOUT_H + 4)
+        gyro_x = read_i2c_word(MPU6050_REGISTER_ACCEL_XOUT_H + 8)
+        gyro_y = read_i2c_word(MPU6050_REGISTER_ACCEL_XOUT_H + 10)
+        gyro_z = read_i2c_word(MPU6050_REGISTER_ACCEL_XOUT_H + 12)
+
+        # Print sensor values
+        print(f"Accelerometer (X, Y, Z): {accel_x}, {accel_y}, {accel_z}")
+        print(f"Gyroscope (X, Y, Z): {gyro_x}, {gyro_y}, {gyro_z}")
+
+        # Delay for some time (e.g., 1 second)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Measurement stopped by the user.")
