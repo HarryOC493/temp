047f5a154470e414d69c4187136f9f365b47a570
Harry O'Connor <harryoconnor007@gmail.com>
2023-10-01 12:10:24 +0100

    Update read.py

--- read.py
+++ read.py
@@ -1,46 +1,17 @@
-import smbus
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
 import time
+import board
+import adafruit_mpu6050
 
-# Define constants
-I2C_BUS = 1  # Use bus 1 for I2C communication on the Jetson Nano
-MPU6050_ADDRESS = 0x68  # MPU6050 I2C address
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+mpu = adafruit_mpu6050.MPU6050(i2c)
 
-# MPU6050 register addresses
-MPU6050_REGISTER_ACCEL_XOUT_H = 0x3B
-MPU6050_REGISTER_PWR_MGMT_1 = 0x6B
-
-# Initialize I2C bus
-i2c = smbus.SMBus(I2C_BUS)
-
-# Function to read 16-bit signed integer (two's complement) from MPU6050
-def read_i2c_word(register):
-    high_byte = i2c.read_byte_data(MPU6050_ADDRESS, register)
-    low_byte = i2c.read_byte_data(MPU6050_ADDRESS, register + 1)
-    value = (high_byte << 8) | low_byte
-    if value >= 0x8000:
-        return -((65535 - value) + 1)
-    else:
-        return value
-
-# Configure MPU6050
-i2c.write_byte_data(MPU6050_ADDRESS, MPU6050_REGISTER_PWR_MGMT_1, 0)
-
-try:
-    while True:
-        # Read accelerometer and gyroscope data
-        accel_x = read_i2c_word(MPU6050_REGISTER_ACCEL_XOUT_H)
-        accel_y = read_i2c_word(MPU6050_REGISTER_ACCEL_XOUT_H + 2)
-        accel_z = read_i2c_word(MPU6050_REGISTER_ACCEL_XOUT_H + 4)
-        gyro_x = read_i2c_word(MPU6050_REGISTER_ACCEL_XOUT_H + 8)
-        gyro_y = read_i2c_word(MPU6050_REGISTER_ACCEL_XOUT_H + 10)
-        gyro_z = read_i2c_word(MPU6050_REGISTER_ACCEL_XOUT_H + 12)
-
-        # Print sensor values
-        print(f"Accelerometer (X, Y, Z): {accel_x}, {accel_y}, {accel_z}")
-        print(f"Gyroscope (X, Y, Z): {gyro_x}, {gyro_y}, {gyro_z}")
-
-        # Delay for some time (e.g., 1 second)
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    print("Measurement stopped by the user.")
+while True:
+    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
+    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
+    print("Temperature: %.2f C" % mpu.temperature)
+    print("")
+    time.sleep(1)
