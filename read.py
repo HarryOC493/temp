8ed035c21c60df2502bed7bd88c24eff7a9e069c
Harry O'Connor <harryoconnor007@gmail.com>
2023-10-09 19:57:38 +0100

    Update read.py

--- read.py
+++ read.py
@@ -27,9 +27,9 @@ try:
             numeric_values = [float(num) for num in re.findall(r'-?\d+\.\d+', line)]
             
             if len(numeric_values) == 3:
-                # Convert angular velocity from deg/s to rad/s
-                angular_velocity = [math.radians(value) for value in numeric_values]
-                print("Angular Velocity (rad/s): X:", angular_velocity[0], "Y:", angular_velocity[1], "Z:", angular_velocity[2])
+                angular_velocity_deg = numeric_values
+                angular_velocity_rad = [round(math.radians(deg), 3) for deg in angular_velocity_deg]
+                print("Angular Velocity (rad/s): X:", angular_velocity_rad[0], "Y:", angular_velocity_rad[1], "Z:", angular_velocity_rad[2])
 
 except KeyboardInterrupt:
     print("Keyboard interrupt detected. Exiting...")
