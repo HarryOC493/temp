df24acc163a893394dedc28cee07bc607dc3f28a
Harry O'Connor <harryoconnor007@gmail.com>
2023-10-09 19:20:36 +0100

    Update read.py

--- read.py
+++ read.py
@@ -4,10 +4,10 @@ import serial
 arduino_port = "/dev/ttyACM0"  # Linux example, may vary on Windows or macOS
 
 # Initialize serial communication with the Arduino
-ser = serial.Serial(arduino_port, baudrate=115200, timeout=1)
+ser = serial.Serial(arduino_port, baudrate=9600, timeout=1)
 
-linear_acceleration = []
-angular_velocity = []
+linear_acceleration_data = []
+angular_velocity_data = []
 
 try:
     while True:
@@ -19,13 +19,13 @@ try:
             # Extract linear acceleration values
             values = line.split('\t')
             linear_values = [float(val.split(': ')[1]) for val in values[1:4]]
-            linear_acceleration.append(linear_values)
+            linear_acceleration_data.append(linear_values)
         
         elif "Angular Velocity" in line:
             # Extract angular velocity values
             values = line.split('\t')
             angular_values = [float(val.split(': ')[1]) for val in values[1:4]]
-            angular_velocity.append(angular_values)
+            angular_velocity_data.append(angular_values)
 
 except KeyboardInterrupt:
     print("Keyboard interrupt detected. Exiting...")
@@ -35,10 +35,10 @@ finally:
     ser.close()
 
 # Print the collected data
-print("Linear Acceleration:")
-for data in linear_acceleration:
+print("Linear Acceleration Data:")
+for data in linear_acceleration_data:
     print(data)
 
-print("Angular Velocity:")
-for data in angular_velocity:
+print("Angular Velocity Data:")
+for data in angular_velocity_data:
     print(data)
