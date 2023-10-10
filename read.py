886259c9765f3a90088c715b15f4b493c45dc1a5
Harry O'Connor <harryoconnor007@gmail.com>
2023-10-10 21:30:54 +0100

    Update read.py

--- read.py
+++ read.py
@@ -32,7 +32,7 @@ try:
                 print("Angular Velocity (rad/s): X:", angular_velocity_rad[0], "Y:", angular_velocity_rad[1], "Z:", angular_velocity_rad[2])
 
         elif "Heading (degrees)" in line:
-            print(line)
+            print(line[19:])
             numeric_values = [float(num) for num in re.findall(r'-?\d+\.\d+', line)]
             print(numeric_values)
             heading_deg = float(numeric_values)
