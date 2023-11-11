51bda98a0e67ba6e5691a4575b42453128093be8
Harry O'Connor <harryoconnor007@gmail.com>
2023-11-11 17:58:44 +0000

    Update velocity.h

--- velocity.h
+++ velocity.h
@@ -1,6 +1,6 @@
 // Pot Mappings. Format VMax | VMin | AMax | AMin | PIN | PrevPosition | Prev Time
 // L_Thigh
-float L_Thigh[7] = {1.86, 0.50, 60, -90, A0, 0, 0}; // Added 2 elements for prevPosition and prevTime
+float L_Thigh[7] = {1.86, 0.50, 60, -90, A0, 0, 0};
 // R_Thigh
 float R_Thigh[7] = {2.65, 0.56, 90, -90, A3, 0, 0};
 
@@ -16,23 +16,21 @@ float R_Ankle[7] = {1.33, 1.97, 25, -40, A5, 0, 0};
 
 void setup() {
   Serial.begin(9600); // Initialize serial communication for debugging
+  // Print the table header
+  Serial.println("Joint\tVMax\tVMin\tVoltage");
 }
 
 float Read_Angle(float joint[7]) {
   int sensorValue = analogRead((int)joint[4]);
-
-  // Map the analog reading to the angle range, considering voltage offsets
   float voltage = sensorValue * (3.3 / 1023.0); // Convert to voltage (0-5V)
   int angle = int(fmap(voltage, joint[1], joint[0], joint[3], joint[2]));
 
-  // Calculate velocity
-  float deltaTime = (millis() - joint[6]) / 1000.0; // Time in seconds
+  float deltaTime = (millis() - joint[6]) / 1000.0;
   float deltaAngle = angle - joint[5];
-  float velocity = deltaAngle / deltaTime; // Angular velocity in degrees per second
+  float velocity = deltaAngle / deltaTime;
 
-  // Update the stored previous position and time
-  joint[5] = angle;      // Update prevPosition
-  joint[6] = millis();  // Update prevTime
+  joint[5] = angle;
+  joint[6] = millis();
 
   // Print the angle and velocity to the serial monitor for debugging
   Serial.print("Potentiometer Angle: ");
@@ -46,31 +44,20 @@ float Read_Angle(float joint[7]) {
   return angle;
 }
 
-void printSensorVoltages() {
-  Serial.println("Sensor\tVMax\tVMin\tVoltage");
-  printVoltage(L_Thigh);
-  printVoltage(R_Thigh);
-  printVoltage(L_Knee);
-  printVoltage(R_Knee);
-  printVoltage(L_Ankle);
-  printVoltage(R_Ankle);
-  Serial.println(); // Blank line for readability
-}
-
 void printVoltage(float joint[7]) {
   int sensorValue = analogRead((int)joint[4]);
-  float voltage = sensorValue * (3.3 / 1023.0); // Convert to voltage (0-5V)
-  Serial.print("Joint "); Serial.print((int)joint[4]); // Print joint number
+  float voltage = sensorValue * (3.3 / 1023.0);
+  Serial.print("A"); Serial.print((int)joint[4]);
   Serial.print("\t");
-  Serial.print(joint[0], 2); // Print VMax
+  Serial.print(joint[0], 2);
   Serial.print("\t");
-  Serial.print(joint[1], 2); // Print VMin
+  Serial.print(joint[1], 2);
   Serial.print("\t");
-  Serial.println(voltage, 2); // Print current voltage
+  Serial.println(voltage, 2);
 }
 
 void loop() {
-  // Update angles and velocities for each joint
+  // Read and print angles and velocities for each joint
   Read_Angle(L_Thigh);
   Read_Angle(R_Thigh);
   Read_Angle(L_Knee);
@@ -78,8 +65,15 @@ void loop() {
   Read_Angle(L_Ankle);
   Read_Angle(R_Ankle);
 
-  // Print the sensor voltages in a table format
-  printSensorVoltages();
+  // Print voltage readings in a table format
+  printVoltage(L_Thigh);
+  printVoltage(R_Thigh);
+  printVoltage(L_Knee);
+  printVoltage(R_Knee);
+  printVoltage(L_Ankle);
+  printVoltage(R_Ankle);
+
+  Serial.println(); // Blank line for readability
 
   delay(1000); // Delay for a second (adjust as needed)
 }
